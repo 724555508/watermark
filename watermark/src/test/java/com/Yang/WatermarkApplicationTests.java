a84a895a70c48534a69bb4221ec46b55804acdc5
Aroli <yanhuakang@iandun.cn>
2020-04-03 18:00:14 +0800

    1

--- watermark/src/test/java/com/Yang/WatermarkApplicationTests.java
+++ watermark/src/test/java/com/Yang/WatermarkApplicationTests.java
@@ -1,13 +1,16 @@
 package com.Yang;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import cn.hutool.http.HttpUtil;
 
 @SpringBootTest
 class WatermarkApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+	
 }
